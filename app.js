0eff4ca728d0f182b5a46aa7a53a949f915170aa
Tracey Boydston <tracey@optimalprime.com>
2023-02-05 09:03:17 +0800

    Modify tests

---
+++ app.js
@@ -0,0 +1,222 @@
+const express = require("express");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const sigMan = require("./lib/signatureManager");
+
+const config = JSON.parse(process.env.CONFIG);
+
+const depositDataPath =
+  process.env.IS_TEST === "true"
+    ? "./tests/data/depositData.json"
+    : config.dataPath;
+
+const pubKey = process.env.PUB_KEY;
+
+const app = express();
+
+const depositData = JSON.parse(fs.readFileSync(depositDataPath));
+
+app.use(
+  bodyParser.text({
+    type() {
+      return "text";
+    },
+  })
+);
+
+// Validate signature on all reqs.
+app.use(async (req, res, next) => {
+  try {
+    const sigResult = await sigMan.verify(
+      pubKey,
+      req.body,
+      req.headers.signature
+    );
+    if (sigResult) return next();
+    console.log("Invalid signature.");
+    res.status(403).send({
+      status: "fail",
+      message: "Invalid signature.",
+    });
+  } catch (e) {
+    res.status(403).send({
+      status: "fail",
+      message: "Signature or data invalid.",
+    });
+  }
+  return true;
+});
+
+// Validate nonce on all reqs.
+app.use(async (req, res, next) => {
+  const now = Date.now();
+  const { nonce } = JSON.parse(req.body);
+
+  if (nonce === undefined || nonce < now - config.nonceTolerance) {
+    console.log(
+      `Invalid nonce. Sent: ${nonce}, No later then: ${
+        now - config.nonceTolerance
+      }`
+    );
+    res.status(403).send({
+      status: "fail",
+      message: "Invalid nonce.",
+    });
+    return true;
+  }
+
+  return next();
+});
+
+// Log incoming req to console.
+app.use(async (req, res, next) => {
+  console.log(req.originalUrl, req.body);
+  next();
+});
+
+// Add deposits.
+app.post("/addresses", async (req, res) => {
+  const { data } = JSON.parse(req.body);
+
+  if (
+    data === undefined ||
+    data.coin === undefined ||
+    data.addresses === undefined ||
+    data.addresses.length === 0 ||
+    depositData[data.coin] === undefined
+  ) {
+    console.log(`Invalid address req. Received: ${JSON.stringify(data)}`);
+    res.status(403).send({
+      status: "fail",
+      message: "Invalid address data.",
+    });
+    return true;
+  }
+
+  try {
+    if (depositData[data.coin].addresses.length > 0) {
+      data.addresses.forEach((newAddress) => {
+        let addressExists = false;
+        depositData[data.coin].addresses.forEach((oldAddress) => {
+          if (oldAddress.address === newAddress.address) {
+            addressExists = true;
+          }
+        });
+        if (addressExists === false) {
+          depositData[data.coin].addresses.push(newAddress);
+        }
+      });
+    } else {
+      depositData[data.coin].addresses = data.addresses;
+    }
+    fs.writeFileSync(depositDataPath, JSON.stringify(depositData, null, 2));
+  } catch (e) {
+    console.log(`Error adding new addresses: ${e.message}`);
+    res.status(403).send({
+      status: "fail",
+      message: `Error adding new addresses: ${e.message}`,
+    });
+    return true;
+  }
+
+  res.status(200).send({
+    status: "success",
+    message: null,
+    data: null,
+  });
+
+  return true;
+});
+
+// Add deposits.
+app.post("/deposits", async (req, res) => {
+  const { data } = JSON.parse(req.body);
+
+  if (
+    data === undefined ||
+    data.coin === undefined ||
+    data.chainHeight === undefined ||
+    data.txData === undefined ||
+    depositData[data.coin] === undefined
+  ) {
+    console.log(`Invalid deposits req. Received: ${JSON.stringify(data)}`);
+    res.status(403).send({
+      status: "fail",
+      message: "Invalid deposits data.",
+    });
+    return true;
+  }
+
+  try {
+    if (depositData[data.coin].deposits.length > 0) {
+      data.txData.forEach((newDeposit) => {
+        let depositExists = false;
+        for (let i = 0; i < depositData[data.coin].deposits.length; i += 1) {
+          const oldDeposit = depositData[data.coin].deposits[i];
+          if (
+            oldDeposit.address === newDeposit.address &&
+            oldDeposit.txid === newDeposit.txid
+          ) {
+            depositData[data.coin].deposits[i].confirmations =
+              newDeposit.confirmations;
+            depositExists = true;
+          }
+        }
+        if (depositExists === false) {
+          depositData[data.coin].deposits.push(newDeposit);
+        }
+      });
+    } else {
+      depositData[data.coin].deposits = data.txData;
+    }
+
+    let highestDepositBlock = 0;
+
+    depositData[data.coin].deposits.forEach((deposit) => {
+      if (deposit.block > highestDepositBlock) {
+        highestDepositBlock = deposit.block;
+      }
+    });
+
+    depositData[data.coin].deposits = trimArray(
+      depositData[data.coin].deposits,
+      config.maxDeposits
+    );
+
+    if (highestDepositBlock > depositData[data.coin].highestDepositBlock) {
+      depositData[data.coin].lastBlockTime = Date.now();
+    }
+
+    depositData[data.coin].highestDepositBlock = highestDepositBlock;
+    depositData[data.coin].lastDepositTime = Date.now();
+    depositData[data.coin].chainHeight = data.chainHeight;
+
+    fs.writeFileSync(depositDataPath, JSON.stringify(depositData, null, 2));
+  } catch (e) {
+    console.log(`Error adding new deposits: ${e.message}`);
+    res.status(403).send({
+      status: "fail",
+      message: `Error adding new deposits: ${e.message}`,
+    });
+    return true;
+  }
+
+  res.status(200).send({
+    status: "success",
+    message: null,
+    data: null,
+  });
+
+  return true;
+});
+
+function trimArray(arr, length) {
+  if (arr.length <= length) {
+    return arr;
+  }
+  arr.sort((a, b) => a - b);
+  arr.splice(0, arr.length - length);
+  return arr;
+}
+
+module.exports = app;
---
+++ tests/server.test.js
@@ -0,0 +1,143 @@
+const request = require("supertest");
+const httpStatus = require("http-status");
+const fs = require("fs");
+
+process.env.IS_TEST = true;
+process.env.PUB_KEY = fs.readFileSync("./tests/keys/pub.pem");
+process.env.CONFIG = JSON.stringify(require("./config-test"));
+
+const depositData = {
+  BTC: {
+    chainHeight: 0,
+    highestDepositBlock: 0,
+    lastBlockTime: 0,
+    lastDepositTime: 0,
+    addresses: {},
+    deposits: [],
+  },
+};
+
+fs.writeFileSync(
+  "./tests/data/depositData.json",
+  JSON.stringify(depositData, null, 2),
+  (error) => {
+    if (error) {
+      console.error(error);
+    }
+  }
+);
+
+const app = require("../app");
+const sigMan = require("../lib/signatureManager");
+
+const privKey = fs.readFileSync("./tests/keys/priv.key");
+
+const demoAddresses = {
+  coin: "BTC",
+  addresses: [
+    {
+      xPubHash:
+        "93f50d6676288093c9f58becad57fd65aa12ae70ccd961039dec0ebb080a9868",
+      index: 1,
+      path: "m/84'/0'/0'/0/1",
+      address: "bc1qcp0t2g0s7m6gul7wmuq4q4xjusrz7q5uhutest",
+      pubKey:
+        "02449dbf873fc99dd194953ab34a076982a1cb014b54063c54c533edd9ee5test",
+    },
+    {
+      xPubHash:
+        "93f50d6676288093c9f58bec4e57fd65aa12ae70ccd961039dec0ebb080a9868",
+      index: 2,
+      path: "m/84'/0'/0'/0/2",
+      address: "bc1qgl88uxk5d9bmnrvru0ww0th35uzcwv3jytest2",
+      pubKey:
+        "03a8a72147af8ac933a6f6d2d2ede6172ea0a3187eb33a707a5fd1f993b65test",
+    },
+  ],
+};
+
+const demoDeposits = {
+  coin: "BTC",
+  chainHeight: 739526,
+  txData: [
+    {
+      xPubHash:
+        "93f50d6676288093c9f58bec4e57fd65aa12ae70ccd961039dec0ebb080a9868",
+      address: "bc1q46y8dujhdy4wl52m4xlkdarp6uvlj280rtest1",
+      amount: 1.0201,
+      confirmations: 0,
+      block: 739432,
+      txid: "bf0980ef2b56288570535f6e34e84da57704657a0aa77669c34a48dcatesttest5",
+    },
+    {
+      xPubHash:
+        "93f50d6676288093c9f58bec4e57fd65aa12ae70ccd961039dec0ebb080a9868",
+      address: "bc1q46y8dujhsy4wl52m4xlkdarp6uvlj280rktest",
+      amount: 1.0001,
+      confirmations: 0,
+      block: 739526,
+      txid: "bf0980ef2b56288570535f6e34e84da57704657a0aa77669c34a48dcadfftest",
+    },
+    {
+      xPubHash:
+        "93f50d6676288093c9f58bec4e57fd65aa12ae70ccd961039dec0ebb080a9868",
+      address: "bc1ql2vsmnjmzkj7adqkf4d9hehgfmj4c8a8vstest",
+      amount: 2.3422,
+      confirmations: 1,
+      block: 739527,
+      txid: "541d5beb1f5a79bcfbebfbfab0f30dcc0595e73f15c335ada78bc70197ectest",
+    },
+  ],
+};
+
+describe("POST /addresses", () => {
+  test("Should add new addresses to tests/data/depositData.json", async () => {
+    const unsignedData = {
+      nonce: Date.now(),
+      data: demoAddresses,
+    };
+
+    const signedData = await sigMan.sign(privKey, JSON.stringify(unsignedData));
+
+    await request(app)
+      .post("/addresses")
+      .set("Signature", signedData.signature)
+      .send(JSON.parse(signedData.data))
+      .expect(httpStatus.OK);
+  });
+});
+
+describe("POST /deposits", () => {
+  test("Should add new deposits to tests/data/depositData.json", async () => {
+    const unsignedData = {
+      nonce: Date.now(),
+      data: demoDeposits,
+    };
+
+    const signedData = await sigMan.sign(privKey, JSON.stringify(unsignedData));
+
+    await request(app)
+      .post("/deposits")
+      .set("Signature", signedData.signature)
+      .send(JSON.parse(signedData.data))
+      .expect(httpStatus.OK);
+  });
+});
+
+describe("Verify Data", () => {
+  test("Should add new deposits to tests/data/depositData.json", async () => {
+    const depositDataFile = JSON.parse(
+      fs.readFileSync("./tests/data/depositData.json")
+    ).BTC;
+
+    expect(depositDataFile.addresses.length).toBe(
+      demoAddresses.addresses.length
+    );
+    expect(depositDataFile.deposits.length).toBe(2);
+    expect(depositDataFile.chainHeight).toBe(739526);
+    expect(depositDataFile.lastBlockTime).toBeGreaterThan(0);
+    expect(depositDataFile.lastDepositTime).toBeGreaterThan(0);
+  });
+});
+
+describe("POST /deposits", () => {});
