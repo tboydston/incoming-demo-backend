2fa0ac110dff76b01dcb8d618f1c2d11ec2d4118
Tracey Boydston <tracey@optimalprime.com>
2023-02-05 07:04:00 +0000

    Fix config json parse issue.

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const fs = require("fs");
 
 if (process.env.IS_TEST === "true") {
   process.env.PUB_KEY = fs.readFileSync("./tests/keys/pub.pem");
-  process.env.CONFIG = require("./tests/config-test");
+  process.env.CONFIG = JSON.stringify(require("./tests/config-test"));
 } else {
   process.env.PUB_KEY = fs.readFileSync("./keys/pub.pem");
-  process.env.CONFIG = require("./config");
+  process.env.CONFIG = JSON.stringify(require("./config"));
 }
 
 const app = require("./app");
 
-const config = JSON.parse(process.env.CONFIG);
+const config = process.env.CONFIG;
 
 try {
   http.createServer(app).listen(config.serverPort);
