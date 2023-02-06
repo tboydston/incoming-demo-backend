f621d02dc9331cb72e8e917843b35696d389090f
Tracey Boydston <tracey@optimalprime.com>
2023-02-06 13:19:20 +0800

    Fix data load order issue.

--- server.js
+++ server.js
@@ -9,23 +9,21 @@ if (process.env.IS_TEST === "true") {
   process.env.CONFIG = JSON.stringify(require("./config"));
 }
 
-const app = require("./app");
-
 const config = JSON.parse(process.env.CONFIG);
 
-(async () => {
-  if (!fs.existsSync(config.dataPath)) {
-    console.log(
-      `Deposit data doesn't exist at ${config.dataPath}. Attempting to create it from template...`
-    );
-    fs.copyFileSync("./data/depositData-template.json", config.dataPath);
-  }
+if (!fs.existsSync(config.dataPath)) {
+  console.log(
+    `Deposit data doesn't exist at ${config.dataPath}. Attempting to create it from template...`
+  );
+  fs.copyFileSync("./data/depositData-template.json", config.dataPath);
+}
 
-  try {
-    http.createServer(app).listen(config.port);
-    console.log(`Demo Server running on port ${config.port}`);
-    console.log(`Config:`, config);
-  } catch (e) {
-    console.log(e);
-  }
-})();
+const app = require("./app");
+
+try {
+  http.createServer(app).listen(config.port);
+  console.log(`Demo Server running on port ${config.port}`);
+  console.log(`Config:`, config);
+} catch (e) {
+  console.log(e);
+}
